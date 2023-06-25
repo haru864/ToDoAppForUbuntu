e662fbc269b7c1fb4e5545b7c4822b0c47302483
haru <kawachigenji95@gmail.com>
2023-06-25 21:28:23 +0900

    scroll task list by mouse wheel

--- src/ToDoApp.py
+++ src/ToDoApp.py
@@ -6,11 +6,31 @@ taskList = []
 
 def addNewTask():
     taskNumber = len(taskList) + 1
-    newTaskButton = Button(innerTaskListFrame, text="Task ")
+    newTaskButton = Button(innerTaskListFrame, text="Task " + str(taskNumber))
     newTaskButton.pack(fill=X)
     taskList.append(newTaskButton)
 
 
+def _on_mousewheel(event):
+    # canvasの現在のスクロール範囲を取得
+    scrollregion = taskListCanvas.cget("scrollregion")
+
+    if scrollregion == "":
+        return
+
+    scrollregion = [int(val) for val in scrollregion.split()]
+    visible_height = taskListCanvas.winfo_height()  # taskListCanvasの可視領域の高さ
+
+    # スクロール可能範囲とtaskListCanvasの可視領域の高さを比較
+    if (scrollregion[3] - scrollregion[1]) > visible_height:
+        # スクロールバーを操作
+        if event.num == 4:
+            taskListCanvas.yview_scroll(-1, "units")
+        elif event.num == 5:
+            taskListCanvas.yview_scroll(1, "units")
+
+
+# ウィンドウを生成
 root = Tk()
 root.title("ToDoApp")
 root.geometry("500x500")
@@ -38,6 +58,8 @@ innerTaskListFrame.bind(
     "<Configure>",
     lambda e: taskListCanvas.configure(scrollregion=taskListCanvas.bbox("all")),
 )
+taskListCanvas.bind("<Button-4>", _on_mousewheel)
+taskListCanvas.bind("<Button-5>", _on_mousewheel)
 
 # メニューにボタンを設置
 addTaskButton = Button(menuFrame, text="add task", command=addNewTask)
